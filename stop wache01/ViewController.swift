9b0df183fd31ff6f6189869cc6f5031297ffed7d
D7703_07 <d7703_07@lulzsec.local>
2019-04-03 15:20:45 +0900

    stop watch

--- stop wache01/ViewController.swift
+++ stop wache01/ViewController.swift
@@ -9,12 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+   
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    var count = 0
+    var myTimer = Timer()
+    var timer_running = false
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        timeLabel.text = String(count)
+    
     }
-
-
+    @IBAction func starBtnPressed(_ sender: Any) {
+        myTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
+        timer_running = true
+    }
+    
+    @IBAction func pauseBtnPressed(_ sender: Any) {
+        myTimer.invalidate()
+    }
+    @IBAction func stopBtnPressed(_ sender: Any) {
+        myTimer.invalidate()
+        count = 0
+        timeLabel.text = String(count)
+    }
+    @objc func updateTime(){
+        count += 1
+        timeLabel.text = String(count)
+    }
+    
 }
 
